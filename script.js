ed78f1e34f2adf6a03474a5ed01bed2b30bc5a14
RujaSubba <rujasubba123@gmail.com>
2024-06-02 18:31:13 +0930

    javascript added

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function calculation(operation) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+
+    let result;
+
+        switch(operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                result = 'Invalid operation';
+        }
+    
+
+    document.getElementById('result').textContent = result;
+}
